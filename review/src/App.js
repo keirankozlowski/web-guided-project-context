bbd9ef026fab0ed7164b8271f163b1c933926804
Keiran Kozlowski <keirankozlowski@gmail.com>
2021-07-15 11:41:06 -0400

    initial commit

--- review/src/App.js
+++ review/src/App.js
@@ -1,27 +1,23 @@
 import React from 'react';
 
-const App = ({ person }) => {
-    console.log(person);
+const App = ()=> {
     return(<div className="App component">
-        <h1>Main Component</h1>
-        <h1>{person.name.title} {person.name.first} {person.name.last}</h1>
-        <SubComp1 person={person} />
+        <h1>Main App</h1>
+        <SubComp1 />
     </div>);
 };
 
-const SubComp1 = ({ person })=> {
-    console.log(person);
+const SubComp1 = ()=> {
     return(<div className="component">
         <h1>Sub Comp 1</h1>
-        <img src={person.picture.large} alt="super cute" />
-        <SubComp2 email={person.email} />
+        <SubComp2 />
     </div>);
 }
 
-const SubComp2 = ({ email }) => {
+const SubComp2 = ()=> {
     return(<div className="component">
         <h1>Sub Comp 2</h1>
-        <p>{email}</p>
+        <SubComp3 />
     </div>);
 }
 
